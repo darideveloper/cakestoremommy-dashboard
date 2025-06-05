c81afe57a04a2ec4d3ba3f0d5ac6d51d1e3d3ab3
daridev <darideveloper@gmail.com>
2025-06-05 17:23:18 -0600

    fix (content/GalleryImageViewSetTestCase/test_filter_category_single_image): remove loop

--- content/tests/test_views.py
+++ content/tests/test_views.py
@@ -8,13 +8,6 @@ class GalleryImageViewSetTestCase(TestContentViewsBase):
     def setUp(self):
         super().setUp(endpoint="/api/gallery-images/")
 
-        # Test data
-        # Note: parent class already have a category and an image created.
-
-        # Create a second category and add an image to it
-        second_category = self.create_category(name="Second Category")
-        #self.create_gallery_image(category=second_category)
-
     def test_no_category_single_image(self):
         """Validate getting a single image without category"""
         
@@ -30,7 +23,6 @@ class GalleryImageViewSetTestCase(TestContentViewsBase):
         json_data = response.json()
         results = json_data.get("results", [])
         self.assertEqual(len(results), 1)
-
 
     def test_no_category_multiple_images(self):
         """Validate getting multiple images without category"""
@@ -75,15 +67,16 @@ class GalleryImageViewSetTestCase(TestContentViewsBase):
         json_data = response.json()
         results = json_data.get("results", [])
         self.assertEqual(len(results), 1)
-        for image in results:
-            # Get image instance
-            image_instance = models.GalleryImage.objects.get(id=image["id"])
-            
-            # Validate image data
-            self.assertEqual(image_instance.description, image["description"])
-            self.assertEqual(image_instance.categories.count(), 1)
-            self.assertEqual(image_instance.categories.first().id, self.category.id)
-            self.assertIn(image_instance.image.url, image["image"])
+        
+        # Get image instance
+        image = results[0]
+        image_instance = models.GalleryImage.objects.get(id=image["id"])
+
+        # Validate image data
+        self.assertEqual(image_instance.description, image["description"])
+        self.assertEqual(image_instance.categories.count(), 1)
+        self.assertEqual(image_instance.categories.first().id, self.category.id)
+        self.assertIn(image_instance.image.url, image["image"])
 
     def test_filter_category_multiple_images(self):
         """Validate getting multiple images with category filter"""
@@ -127,7 +120,6 @@ class GalleryImageViewSetTestCase(TestContentViewsBase):
 
     def test_invalid_category(self):
         """Validate getting no images with an invalid category"""
-        """Validate getting no images with category filter"""
         # Get api data
         response = self.client.get(self.endpoint, {"category": 584})
         
