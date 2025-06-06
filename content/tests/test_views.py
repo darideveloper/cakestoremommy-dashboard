532b662294bde36a6bec71df36d1f61ebdfb5a0b
Abel Soto Valdez <abelsotovaldez@MacBook-Air-de-Abel.local>
2025-06-05 23:07:23 -0600

    perf(test_views): validate attributes on recover image

--- content/tests/test_views.py
+++ content/tests/test_views.py
@@ -8,6 +8,16 @@ class GalleryImageViewSetTestCase(TestContentViewsBase):
     def setUp(self):
         super().setUp(endpoint="/api/gallery-images/")
 
+    def validate_image_attributes(self, image):
+        # Get image instance
+        image_instance = models.GalleryImage.objects.get(id=image["id"])
+
+        # Validate image data
+        self.assertEqual(image_instance.description, image["description"])
+        self.assertEqual(image_instance.categories.count(), 1)
+        self.assertEqual(image_instance.categories.first().id, self.category.id)
+        self.assertIn(image_instance.image.url, image["image"])
+
     def test_no_category_single_image(self):
         """Validate getting a single image without category"""
         
@@ -24,6 +34,9 @@ class GalleryImageViewSetTestCase(TestContentViewsBase):
         results = json_data.get("results", [])
         self.assertEqual(len(results), 1)
 
+        # Validate image attributes
+        self.validate_image_attributes(results[0])
+
     def test_no_category_multiple_images(self):
         """Validate getting multiple images without category"""
         self.create_gallery_image(category=self.category, description="Image a")
@@ -39,6 +52,10 @@ class GalleryImageViewSetTestCase(TestContentViewsBase):
         json_data = response.json()
         results = json_data.get("results", [])
         self.assertEqual(len(results), 2)
+
+        for image in results:
+            # Validate image attributes
+            self.validate_image_attributes(image)
 
     def test_no_category_no_images(self):
         """Validate getting no images without category"""
@@ -68,15 +85,8 @@ class GalleryImageViewSetTestCase(TestContentViewsBase):
         results = json_data.get("results", [])
         self.assertEqual(len(results), 1)
         
-        # Get image instance
-        image = results[0]
-        image_instance = models.GalleryImage.objects.get(id=image["id"])
-
-        # Validate image data
-        self.assertEqual(image_instance.description, image["description"])
-        self.assertEqual(image_instance.categories.count(), 1)
-        self.assertEqual(image_instance.categories.first().id, self.category.id)
-        self.assertIn(image_instance.image.url, image["image"])
+        # Validate image attributes
+        self.validate_image_attributes(results[0])
 
     def test_filter_category_multiple_images(self):
         """Validate getting multiple images with category filter"""
@@ -95,15 +105,10 @@ class GalleryImageViewSetTestCase(TestContentViewsBase):
         json_data = response.json()
         results = json_data.get("results", [])
         self.assertEqual(len(results), 2)
+
         for image in results:
-            # Get image instance
-            image_instance = models.GalleryImage.objects.get(id=image["id"])
-            
-            # Validate image data
-            self.assertEqual(image_instance.description, image["description"])
-            self.assertEqual(image_instance.categories.count(), 1)
-            self.assertEqual(image_instance.categories.first().id, self.category.id)
-            self.assertIn(image_instance.image.url, image["image"])
+            # Validate image attributes
+            self.validate_image_attributes(image)
 
     def test_filter_category_no_images(self):
         """Validate getting no images with category filter"""
@@ -133,4 +138,5 @@ class GalleryImageViewSetTestCase(TestContentViewsBase):
     
     def test_single_image_many_categories(self):
         """Validate getting a single image with multiple categories"""
+        
         pass
